6c1d0342de606b1c961f8839ace43be31f64ebe4
Mario Ramirez <mario11842@hotmail.com>
2021-10-30 11:33:51 -0600

    Mejoras

--- src/com/Project/Main.java
+++ src/com/Project/Main.java
@@ -8,13 +8,57 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Scanner opcMenu = new Scanner(System.in);
 
+        System.out.println("Bienvenido al sistema");
+        System.out.println("Ingrese en que tabla desea registrar datos: ");
+        System.out.println("1. Tabla Huesped");
+
+        int opcionMenu = opcMenu.nextInt();
+        menu(opcionMenu);
+    }
+
+    public static void menu(int opc){
+        int opcion = opc;
+        do {
+            switch (opcion) {
+                case 1:
+                    opc = 4;
+                    subMenu();
+                    break;
+                default:
+                    System.out.println("¡OPCION INCORRECTA!");
+                    opc = 4;
+            }
+        }while(opc != 4);
+    }
+
+    public static void subMenu(){
+        Scanner opcSmenu = new Scanner(System.in);
+
+        System.out.println("Ingrese que operacion desea realizar: ");
+        System.out.println("1. Ver Huespedes.");
+
+        int opcionMenu = opcSmenu.nextInt();
+
+        int opcion = opcionMenu;
+        do {
+            switch (opcion) {
+                case 1:
+                    opcionMenu = 4;
+                    mostrarHuesped();
+                    break;
+                default:
+                    System.out.println("¡OPCION INCORRECTA!");
+                    opcionMenu = 4;
+            }
+        }while(opcionMenu != 4);
+    }
+
+    public static void mostrarHuesped(){
         Connection conexion = null;
-
         String driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver" ;
         String url = "jdbc:sqlserver://MARIORAMIREZ:1433;database=DB_GestionHotel;user= MarioR;password=123;loginTimeout = 30 ";
-
-
         try {
             Class.forName(driver);
             conexion = DriverManager.getConnection(url);
@@ -27,7 +71,7 @@ public class Main {
                 System.out.println("ID|NOMBRES|APELLIDOS|TELEFONO|EDAD|DPI|EMAIL ");
                 while(resultado.next()){
                     System.out.println(resultado.getInt(1)+ " " +resultado.getString(2) + " " +resultado.getString(3)+ " "+resultado.getInt(4)+
-                                            " "+resultado.getInt(5)+" "+resultado.getInt(6)+" "+resultado.getString(7));
+                            " "+resultado.getInt(5)+" "+resultado.getInt(6)+" "+resultado.getString(7));
                 }
             }
 
@@ -38,44 +82,5 @@ public class Main {
             System.out.println("ERROR AL CONECTARSE A LA BASE DE DATOS");
             System.out.println(e.getMessage());
         }
-
-
-
-        //Scanner opcMenu = new Scanner(System.in);
-
-        //System.out.println("Bienvenido al sistema");
-        //System.out.println("Ingrese en que tabla desea registrar datos: ");
-        //System.out.println("1. Tabla Huesped");
-        //System.out.println("2. Tabla empleados");
-        //System.out.println("3. Tabla de Habitaciones");
-
-        //int opcionMenu = opcMenu.nextInt();
-        //menu(opcionMenu);
-
-
-
-    }
-
-    public static void menu(int opc){
-        int opcion = opc;
-        do {
-            switch (opcion) {
-                case 1:
-                    opc = 4;
-                    System.out.println("1");
-                    break;
-                case 2:
-                    opc = 4;
-                    System.out.println("2");
-                    break;
-                case 3:
-                    opc = 4;
-                    System.out.println("3");
-                    break;
-                default:
-                    System.out.println("¡OPCION INCORRECTA!");
-                    opc = 4;
-            }
-        }while(opc != 4);
     }
 }
